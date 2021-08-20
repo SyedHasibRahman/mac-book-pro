d6a0ddf770d23f2383f08fd04531ec0ba67cc7bc
SyedHasibRahman <syedhasib01@gmail.com>
2021-08-21 04:21:32 +0600

    reduce code duplication

--- js/app.js
+++ js/app.js
@@ -1,78 +1,66 @@
-function memoryOption(isIncreasing) {
-    const extraMemory = document.getElementById('memory-option');
-    let extraMemoryPrice = parseInt(extraMemory.innerText);
-    if (isIncreasing == true) {
-        extraMemory.innerText = 180;
-    } else if (isIncreasing == false) {
-        extraMemory.innerText = 0;
+// Main function 
+function memoryOption(product, option) {
+    const extraCharges = document.getElementById(product + '-option');
+    let extraMemoryPrice = parseInt(extraCharges.innerText);
+    // Memory Section 
+    if (option == 'memory-16gb') {
+        extraCharges.innerText = 180;
+    } else if (option == 'memory-8gb') {
+        extraCharges.innerText = 0;
     }
-    extraMemoryPrice = parseInt(extraMemory.innerText);
+    // delivery Section 
+    if (option == 'argent-delivery') {
+        extraCharges.innerText = 20;
+    } else if (option == 'free-delivery') {
+        extraCharges.innerText = 0;
+    }
+    // Storage Section 
+    if (option == 'ssd-256gb') {
+        extraCharges.innerText = 0;
+    }
+    else if (option == 'ssd-512gb') {
+        extraCharges.innerText = 100;
+    } else if (option == 'ssd-1tb') {
+        extraCharges.innerText = 180;
+    }
     calculateTotal();
     document.getElementById('apply-btn').removeAttribute("disabled", true);
+
+    document.getElementById('promo-text').innerText = 'Add Promo code';
+    document.getElementById('promo-text').style.color = '#333';
 };
 document.getElementById('memory-16gb').addEventListener('click', function () {
-
-    memoryOption(true);
-
+    memoryOption('memory', 'memory-16gb');
 });
 document.getElementById('memory-8gb').addEventListener('click', function () {
-
-    memoryOption(false);
-
+    memoryOption('memory', 'memory-8gb');
 });
-//  Delivery Options 
-function deliveryOptions(deliveryOptions) {
-    const deliveryOption = document.getElementById('delivery-option');
-    let deliveryCharges = deliveryOption.innerText;
-    if (deliveryOptions == 'free-delivery') {
-        deliveryOption.innerText = 0;
-    } else if (deliveryOptions == 'argent-delivery') {
-        deliveryOption.innerText = 20;
-    }
-    deliveryCharges = parseInt(deliveryOption.innerText);
-    calculateTotal();
-    document.getElementById('apply-btn').removeAttribute("disabled", true);
-}
 document.getElementById('free-delivery').addEventListener('click', function () {
-    deliveryOptions('free-delivery');
-})
+    // deliveryOptions('free-delivery');
+    memoryOption('delivery', 'free-delivery');
+});
 document.getElementById('argent-delivery').addEventListener('click', function () {
-    deliveryOptions('argent-delivery');
-})
-
-// ssd Options
-function ssdOption(isIncreasing) {
-    const extrStorage = document.getElementById('storage-option');
-    let extrStoragePrice = extrStorage.innerText;
-    if (isIncreasing == 'ssd-256gb') {
-        extrStorage.innerText = 0;
-    }
-    else if (isIncreasing == 'ssd-512gb') {
-        extrStorage.innerText = 100;
-    } else if (isIncreasing == 'ssd-1tb') {
-        extrStorage.innerText = 180;
-    }
-    extrStoragePrice = parseInt(extrStorage.innerText);
-    calculateTotal();
-    document.getElementById('apply-btn').removeAttribute("disabled", true);
-};
+    memoryOption('delivery', 'argent-delivery');
+});
 document.getElementById('ssd-256gb').addEventListener('click', function () {
-    ssdOption('ssd-256gb');
+    memoryOption('storage', 'ssd-256gb');
 });
 document.getElementById('ssd-512gb').addEventListener('click', function () {
-    ssdOption('ssd-512gb');
+    memoryOption('storage', 'ssd-512gb');
 });
 document.getElementById('ssd-1tb').addEventListener('click', function () {
-    ssdOption('ssd-1tb');
+    memoryOption('storage', 'ssd-1tb');
 
 });
 
+
+// geting Price 
 function getPriceValue(product) {
     const priceText = document.getElementById(product + '-option').innerText;
     const price = parseInt(priceText);
     return price;
 }
-
+// Calculate Total Price 
 function calculateTotal() {
     const extraMemoryPrice = getPriceValue('memory');
     const extrStoragePrice = getPriceValue('storage');
@@ -84,12 +72,12 @@ function calculateTotal() {
 
 }
 
-// Promo code 
+// Promo code options
 document.getElementById('apply-btn').addEventListener('click', function () {
     const promoInput = document.getElementById('promo-input').value;
     const promoPriceText = document.getElementById('promo-price').innerText;
     const promoPrice = parseFloat(promoPriceText);
-    if (promoInput == 'h') {
+    if (promoInput == 'stevekaku') {
         document.getElementById('promo-price').innerText = promoPrice * .8;
         document.getElementById('promo-input').value = '';
         document.getElementById('apply-btn').setAttribute("disabled", true);
